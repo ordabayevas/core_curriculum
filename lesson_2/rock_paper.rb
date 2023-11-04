aec3fb1388d4855bd5e39ef8fb9a9e4d45a26ab3
Saltanat Ordabayeva <ordabayeva.sr@gmail.com>
2023-11-03 21:48:12 -0400

    Updated

--- lesson_2/rock_paper.rb
+++ lesson_2/rock_paper.rb
@@ -16,6 +16,20 @@ COMBINATIONS = {
   'spock' => ['scissors', 'rock']
 }
 
+player_point = 0
+computer_point = 0
+
+def points_counter(winner, player_point, computer_point)
+  if winner == 'player'
+    player_point += 1
+  elsif winner == 'computer'
+    computer_point += 1
+  end
+  prompt("You have #{player_point} points")
+  prompt("Computer has #{computer_point} points")
+  return player_point, computer_point
+end
+
 def win?(first, second)
   COMBINATIONS[first].include?(second)
 end
@@ -23,10 +37,13 @@ end
 def display_results(player, computer)
   if win?(player, computer)
     prompt('You won!')
+    return 'player'
   elsif win?(computer, player)
     prompt('Computer won!')
+    return 'computer'
   else
     prompt("That's a tie!")
+    return 'tie'
   end
 end
 
@@ -54,11 +71,22 @@ loop do
 
   prompt("You chose: #{choice}, computer chose: #{computer_choice}")
 
-  display_results(choice, computer_choice)
+  result = display_results(choice, computer_choice)
 
-  prompt('Do you want to play again?')
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase.start_with?('y')
+  player_point, computer_point = points_counter(result, player_point, computer_point)
+
+  if computer_point == 3
+    prompt('Gamer over. Computer win!')
+    break
+  elsif player_point == 3
+    prompt('Congrats. You win!')
+    break
+  else
+    prompt('Do you want to play again?')
+    answer = Kernel.gets().chomp()
+    break unless answer.downcase.start_with?('y')
+  end
+
 end
 
 prompt('Thank you for playing. Goodbye!')
