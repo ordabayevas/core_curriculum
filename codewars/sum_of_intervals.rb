3c44b2d6faeb5b31c394e330f7bc5efd54a167fc
Saltanat Ordabayeva <ordabayeva.sr@gmail.com>
2023-12-13 11:00:39 -0500

    New Code Wars

--- codewars/sum_of_intervals.rb
+++ codewars/sum_of_intervals.rb
@@ -1,43 +1,93 @@
-def sum_of_intervals(intervals)
-  new_intervals = []
-  intervals.each do |subarray|
-    subarray[1].times do |num|
-      if num < subarray[0]
-        next
-      else
-      new_intervals << num
-      end
-    end
-  end
-  a = new_intervals.uniq.size
+# def sum_of_intervals(intervals)
+#   new_intervals = []
+#   intervals.each do |subarray|
+#     subarray[1].times do |num|
+#       if num < subarray[0]
+#         next
+#       else
+#         new_intervals << num
+#       end
+#     end
+#   end
+#   a = new_intervals.uniq.size
 
-  neg_sub = intervals.select do |sub|
-              sub[0] < 0
-            end
-  if neg_sub.empty?
-    return a
-  else
-    neg_sub = neg_sub.flatten
-    pos_sub = neg_sub[0].abs
-    return result = a + pos_sub
-  end
+#   neg_sub = intervals.select do |sub|
+#               sub[0] < 0
+#             end
+#   if neg_sub.empty?
+#     return a
+#   else
+#     neg_sub = neg_sub.flatten
+#     pos_sub = neg_sub[0].abs
+#     return result = a + pos_sub
+#   end
 
-end
+# end
 
 #p sum_of_intervals([[1, 4], [7, 10], [3, 5]])
 #p sum_of_intervals([[1, 5], [10, 20], [1, 6], [16, 19], [5, 11]])
 #p sum_of_intervals([[0, 20], [-100000000, 10], [30, 40]])
 #p sum_of_intervals([[1, 5], [6, 10]])
+#p sum_of_intervals([[1, 5]])
+
+def is_overlapping(arr1, arr2)
+  total_sum = (arr1[1] - arr1[0]) + (arr2[1] - arr2[0])
+  total_interval = [arr1[1], arr2[1]].max - [arr1[0], arr2[0]].min
+  total_sum >= total_interval
+end
+
+def merge(arr1, arr2)
+  left = [arr1[0], arr2[0]].min
+  right = [arr1[1], arr2[1]].max
+  [left, right]
+end
+
+def non_overlapped(intervals)
+  non_overlapping_inter = []
+  while intervals.length > 0
+    subarray = intervals[0]
+    intervals = intervals[1..-1]
+    if intervals.length == 0
+      non_overlapping_inter << subarray
+    else
+      overlapped = false
+      intervals.each_index do |index|
+        arr = intervals[index]
+        if is_overlapping(subarray, arr)
+          intervals[index] = merge(subarray, arr)
+          overlapped = true
+          next
+        end
+      end
+      if overlapped == false
+        non_overlapping_inter << subarray
+      end
+    end
+    # p non_overlapping_inter
+  end
+  non_overlapping_inter
+end
+
+def sum_of_intervals(array)
+  new_intervals = non_overlapped(array)
+  result = []
+  new_intervals.each do |a|
+    diff = a[1] - a[0]
+    result << diff
+  end
+  sum_result = result.sum
+end
+
+
+
+# p sum_of_intervals([[1, 4], [7, 10], [3, 5]])
+# p sum_of_intervals([[1, 5], [10, 20], [1, 6], [16, 19], [5, 11]])
+# p sum_of_intervals([[0, 20], [-100000000, 10], [30, 40]])
+# p sum_of_intervals([[1, 5], [6, 10]])
 p sum_of_intervals([[1, 5]])
 
 
-
-
-
-
-
-# new_intervals = []
-#   for subarray in intervals do
-#     new_sub = subarray[0]...subarray[1]
-#     new_intervals << new_sub
-#   end
+# better solution
+# def sum_of_intervals(intervals)
+#   intervals.map{ |i| (i[0]...i[1]).to_a }.flatten.uniq.size
+# end
--- small_problems_exercises/example.rb
+++ small_problems_exercises/example.rb
@@ -19,7 +19,180 @@
 
 # puts a.find { |x| x > 8}
 
-a = %w(a b c d e) 
-puts a.fetch(7) # => error
-puts a.fetch(7, 'beats me') # => 'beats me'
-puts a.fetch(7) { |index| index**2 } # => 49
+# a = %w(a b c d e) 
+# puts a.fetch(7) # => error
+# puts a.fetch(7, 'beats me') # => 'beats me'
+# puts a.fetch(7) { |index| index**2 } # => 49
+# num = 5
+# while num != 0
+#   num -= 1
+#   puts num
+# end
+
+# s = 'string'
+# p s[0]
+
+#sum = (1..integer.to_i).to_a.sum
+
+# prod = 1
+# (1..5).to_a.each do |num|
+#   prod *= num
+# end
+
+# p prod
+
+# p 22.to_s == '22'.reverse
+
+# greeting = 'Hello'
+# p greeting.object_id
+
+# loop do
+#   greeting = 'Hi'
+#   p greeting.object_id
+#   break
+# end
+
+# puts greeting
+# p greeting.object_id
+
+# def replace(str, value)
+#   loop do
+#     break
+#   end
+#   str = value
+# end
+
+# greet = 'Hey!'
+# replace(greet, 'Hello')
+# puts greet
+
+# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# new_array = arr.select do |n|
+#    n = n + 1
+#    puts n
+# end
+
+# p new_array
+
+
+# selected_foods = [['apple', 'kiwi'], ['artichoke', 'broccoli']].select do |arr|
+#   puts selected_foods.object_id
+#   arr.select do |food|
+#     food.start_with?('a')
+#   end
+# end
+
+# p selected_foods
+# puts selected_foods.object_id
+
+# def append(s)
+#   d = s.upcase!
+# end
+
+# t = 'abc'
+# puts append(t)
+# puts t
+
+# first_name = 'Winston'
+# last_name = 'Churchill'
+# lower_case_name = first_name + last_name
+# upper_case_name = lower_case_name.upcase
+# puts "This is lowercase #{lower_case_name}"
+# puts "This is uppercase #{upper_case_name}"
+
+# def string(string)
+#   if string.size >= 80
+#     puts string[0...80]
+#   end
+# end
+
+# string('The code range can be used to quickly disqualify strings known not to match, such as those with the')
+# a = ['a', 'b', 'c']
+
+# k = a.select do |letter| 
+#       a.delete_at(-1) 
+#       a.include?(letter)
+#     end
+# k = k.size
+
+# p k
+
+# def say(words='hello')
+#   puts words + '.'
+# end
+
+# say()
+# say("hi")
+# say("how are you")
+# say("I'm fine")
+
+# def some_method
+#   a = 1
+#   b = 0
+#   5.times do
+#     puts a
+#     b = 2
+#   end
+
+#   puts a
+#   puts b
+# end
+
+# some_method     # => NameError: undefined local variable or method `b' for main:Object
+
+# def sum(num1, num2)
+#   sum = num1 + num2
+# end
+
+# a = 3
+# b = 5
+# p sum(a, b) # 8
+# p a # 3
+# p b # 5
+
+arr = [1, 2, 3, 4]
+# odds = arr.select do |n|
+# 	n + 1
+# 	puts n
+# end
+
+# # => []
+# p odds
+
+# incremented = arr.map do |n|
+#   "hi"
+# end
+
+# p incremented
+
+# def repeat(string, number)
+#   number.times do # 3.times do
+#     puts string # puts 'Hello'
+#   end
+# end
+
+# repeat('Hello', 3)
+
+# def stringy(size)
+#   numbers = []
+
+#   size.times do |index|
+#     number = index.even? ? 1 : 0
+#     numbers << number
+#   end
+
+#   numbers.join
+# end
+
+# puts stringy(6) == '101010'
+
+num = 1
+
+loop do |num|
+  puts num
+  num = 2
+  break
+end
+
+puts num # => 1
