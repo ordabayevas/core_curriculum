be6bf4a9164e58ceb7c8dafac82a2312542a2d16
Saltanat Ordabayeva <ordabayeva.sr@gmail.com>
2025-01-02 18:38:13 -0500

    Book Viewer

--- RB_175/lesson_3/book_viewer/book_viewer.rb
+++ RB_175/lesson_3/book_viewer/book_viewer.rb
@@ -9,12 +9,69 @@ require "sinatra/reloader"
 
 set :server, 'webrick'  # Force Sinatra to use WEBrick
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
-  # File.read "public/template.html"
-
   @title = "The Adventures of Sherlock Holmes"
-  @toc = File.readlines "data/toc.txt"
-  
-  erb :home
 
+  erb :home
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  
+  redirect "/" unless (1..@contents.size).cover? number
+  
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
+
+  erb :chapter
+end
+
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+helpers do
+  def in_paragraphs(chapter)
+    chapter.split("\n\n").map.with_index do |paragraph, index|
+      "<p'>#{paragraph}<\p>"
+    end.join
+  end
+
+  def highlight(text, term)
+    text.gsub(term, %(<strong>#{term}</strong>))
+  end
+end
+
+not_found do
+  redirect "/"
 end
